abf9e46401240d598f868cbc53b5be7a04581ed6
yk-lab <yk-lab@users.noreply.github.com>
2025-03-20 15:55:46 +0900

    feat: Initialize project structure with main command and configuration support

---
+++ cmd/root.go
@@ -0,0 +1,69 @@
+package cmd
+
+import (
+	"os"
+	"github.com/yk-lab/toske/utils"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "toske",
+	Short: "A brief description of your application",
+	Long: utils.AAFromText("hero.txt"),
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.toske.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".toske" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".toske")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
---
+++ utils/convert.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"bytes"
+	"fmt"
+	"path/filepath"
+
+	"github.com/yk-lab/toske/static"
+)
+
+func PrintAAFromTxt(fp string) {
+	text := readFile(filepath.Join("aa", fp))
+	fmt.Print(text)
+}
+
+func AAFromText(fp string) string {
+	text := readFile(filepath.Join("aa", fp))
+	return text
+}
+
+func readFile(fp string) string {
+	file, err := static.Aa.Open(fp)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	// ファイルを読み込んで出力
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(file)
+	return buf.String()
+}
